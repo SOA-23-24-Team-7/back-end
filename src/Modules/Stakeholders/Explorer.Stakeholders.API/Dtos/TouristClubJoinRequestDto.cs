ebe01dd7365ae602407702bff430364f1195c363
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-10-15 10:59:23 +0200

    feat: TouristClubJoinRequestDto Class.

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/TouristClubJoinRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class TouristClubJoinRequestDto
+    {
+        public long TouristId { get; set; }
+        public long ClubId { get; set; }
+        public DateTime RequestedAt { get; set; }
+    }
+}
