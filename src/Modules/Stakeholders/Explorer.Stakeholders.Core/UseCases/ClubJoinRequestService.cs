c797d77fc56b9048395b82caab2603cd895ccf9c
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-10-15 11:39:58 +0200

    feat: ClubJoinRequestService Class.

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/UseCases/ClubJoinRequestService.cs
@@ -0,0 +1,35 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.Core.Domain.RepositoryInterfaces;
+using Explorer.Stakeholders.Core.Domain;
+using Explorer.Tours.API.Public.Administration;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.UseCases
+{
+    public class ClubJoinRequestService : IClubJoinRequestService
+    {
+        ClubJoinRequestService()
+        {
+        }
+
+        public Result<ClubJoinRequestDto> Send(ClubJoinRequestDto request)
+        {
+            try
+            {
+                var joinRequest = new ClubJoinRequest(request.TouristId, request.ClubId, request.RequestedAt, ClubJoinRequestStatus.Pending);
+                return request;
+            }
+            catch (ArgumentException e)
+            {
+                return Result.Fail(FailureCode.InvalidArgument).WithError(e.Message);
+            }
+        }
+    }
+}
