725476e4925310976b410f0ccd993743fb836713
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-10-15 11:48:37 +0200

    feat: ClubJoinRequestController Class.

---
+++ src/Explorer.API/Controllers/Tourist/ClubJoinRequestController.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.Stakeholders.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/club-join-request")]
+    public class ClubJoinRequestController : BaseApiController
+    {
+        private readonly IClubJoinRequestService _requestService;
+
+        public ClubJoinRequestController(IClubJoinRequestService requestService)
+        {
+            _requestService = requestService;
+        }
+
+        [HttpPost]
+        public ActionResult<ClubJoinRequestDto> Send([FromBody] ClubJoinRequestDto request)
+        {
+            var result = _requestService.Send(request);
+            return CreateResponse(result);
+        }
+    }
+}
