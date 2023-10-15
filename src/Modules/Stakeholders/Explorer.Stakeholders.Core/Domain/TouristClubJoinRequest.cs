8b9d57cb7fd5b93d06f111bb087b7125d67b7be1
marko-nikolic01 <116906156+marko-nikolic01@users.noreply.github.com>
2023-10-15 10:54:23 +0200

    feat: TouristClubJoinRequest Class.

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/TouristClubJoinRequest.cs
@@ -0,0 +1,42 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.Core.Domain
+{
+    public class TouristClubJoinRequest : Entity
+    {
+        public long TouristId { get; init; }
+        public long ClubId { get; init; }
+        public DateTime RequestedAt { get; init; }
+        public TouristClubJoinRequestStatus Status { get; init; }
+
+        public TouristClubJoinRequest(long touristId, long clubId, DateTime requestedAt, TouristClubJoinRequestStatus status)
+        {
+            TouristId = touristId;
+            ClubId = clubId;
+            RequestedAt = requestedAt;
+            Status = status;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (TouristId == 0) throw new ArgumentException("Invalid TouristId");
+            if (ClubId == 0) throw new ArgumentException("Invalid ClubId");
+            if (RequestedAt > DateTime.Now) throw new ArgumentException("Invalid RequestedAt");
+        }
+    }
+}
+
+public enum TouristClubJoinRequestStatus
+{
+    Pending,
+    Accepted,
+    Rejected,
+    Cancelled
+}
