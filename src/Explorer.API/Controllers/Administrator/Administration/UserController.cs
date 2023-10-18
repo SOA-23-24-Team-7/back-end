387e6b25e2fbc7c634debfb7f2c1696addb99be3
gagi155 <maslac.ra151.2020@gmail.com>
2023-10-18 21:13:58 +0200

    code refactoring

--- src/Explorer.API/Controllers/Administrator/Administration/UserController.cs
+++ src/Explorer.API/Controllers/Administrator/Administration/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Explorer.Stakeholders.API.Dtos;
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
 using Explorer.Stakeholders.API.Public;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -6,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Explorer.API.Controllers.Administrator.Administration
 {
     [Authorize(Policy = "administratorPolicy")]
-    [Route("api/users")]
+    [Route("api/administration/users")]
     public class UserController : BaseApiController
     {
         private readonly IUserService _userService;
@@ -16,10 +17,17 @@ namespace Explorer.API.Controllers.Administrator.Administration
             _userService = userService;
         }
 
-        [HttpPost("block")]
-        public ActionResult<UserDto> DisableAccount([FromBody] long userId)
+        [HttpGet("disable/{userId:long}")]
+        public ActionResult<UserDto> DisableAccount(long userId)
         {
             var result = _userService.DisableAccount(userId);
+            return CreateResponse(result);
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<UserDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _userService.GetPaged(page, pageSize);
             return CreateResponse(result);
         }
     }
--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IUserService.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Public/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using Explorer.Stakeholders.API.Dtos;
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
 using FluentResults;
 
 namespace Explorer.Stakeholders.API.Public
@@ -6,5 +7,6 @@ namespace Explorer.Stakeholders.API.Public
     public interface IUserService
     {
         Result<UserDto> DisableAccount(long userId);
+        Result<PagedResult<UserDto>> GetPaged(int page, int pageSize);
     }
 }
