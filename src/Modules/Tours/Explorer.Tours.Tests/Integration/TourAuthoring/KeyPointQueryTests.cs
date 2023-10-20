8eb593ff312a36b0d7ae45274ebbf74a8019bdea
mvelimir <velimirmilinkovic1@gmail.com>
2023-10-20 21:51:17 +0200

    test: Add tests for creating key points

---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/TourAuthoring/KeyPointQueryTests.cs
@@ -0,0 +1,37 @@
+﻿using Explorer.API.Controllers.Author.TourAuthoring;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.TourAuthoring;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Tours.Tests.Integration.TourAuthoring;
+
+[Collection("Sequential")]
+public class KeyPointQueryTests : BaseToursIntegrationTest
+{
+    public KeyPointQueryTests(ToursTestFactory factory) : base(factory) { }
+
+    [Fact]
+    public void Retrieves_all_for_single_tour()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+
+        // Act
+        var result = ((ObjectResult)controller.GetKeyPoints(-1).Result)?.Value as List<KeyPointDto>;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Count.ShouldBe(2);
+    }
+
+    private static TourController CreateController(IServiceScope scope)
+    {
+        return new TourController(scope.ServiceProvider.GetRequiredService<IKeyPointService>())
+        {
+            ControllerContext = BuildContext("-1")
+        };
+    }
+}
