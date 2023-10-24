6dfc23366343f843586f78faf1857dd5840c5e97
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-10-24 17:11:14 +0200

    Deleted namespaces

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IClubJoinRequestRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IClubJoinRequestRepository.cs
@@ -1,11 +1,5 @@
-﻿using Explorer.BuildingBlocks.Core.Domain;
-using Explorer.BuildingBlocks.Core.UseCases;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Explorer.BuildingBlocks.Core.UseCases;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
 {
