ec31d2bcff9288b91aca12709a0be814afca6406
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-10-24 17:14:24 +0200

    Update ClubJoinRequest.cs

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/ClubJoinRequest.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/ClubJoinRequest.cs
@@ -1,54 +1,55 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
 
-namespace Explorer.Stakeholders.Core.Domain;
-
-public class ClubJoinRequest : Entity
+namespace Explorer.Stakeholders.Core.Domain
 {
-    public long TouristId { get; init; }
-    public Person? Tourist { get; init; }
-    public long ClubId { get; init; }
-    public Club? Club { get; init; }
-    public DateTime RequestedAt { get; init; }
-    public ClubJoinRequestStatus Status { get; private set; }
-
-    public ClubJoinRequest(long touristId, long clubId, DateTime requestedAt, ClubJoinRequestStatus status)
+    public class ClubJoinRequest : Entity
     {
-        TouristId = touristId;
-        ClubId = clubId;
-        RequestedAt = requestedAt;
-        Status = status;
-        Validate();
+        public long TouristId { get; init; }
+        public Person? Tourist { get; init; }
+        public long ClubId { get; init; }
+        public Club? Club { get; init; }
+        public DateTime RequestedAt { get; init; }
+        public ClubJoinRequestStatus Status { get; private set; }
+
+        public ClubJoinRequest(long touristId, long clubId, DateTime requestedAt, ClubJoinRequestStatus status)
+        {
+            TouristId = touristId;
+            ClubId = clubId;
+            RequestedAt = requestedAt;
+            Status = status;
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (TouristId == 0) throw new ArgumentException("Invalid TouristId");
+            if (ClubId == 0) throw new ArgumentException("Invalid ClubId");
+            if (RequestedAt > DateTime.Now) throw new ArgumentException("Invalid RequestedAt");
+        }
+
+        public void Respond(bool accepted)
+        {
+            if (Status == ClubJoinRequestStatus.Pending)
+                Status = accepted ? ClubJoinRequestStatus.Accepted : ClubJoinRequestStatus.Rejected;
+        }
+
+        public void Cancel()
+        {
+            if (Status == ClubJoinRequestStatus.Pending)
+                Status = ClubJoinRequestStatus.Cancelled;
+        }
+
+        public string GetPrimaryStatusName()
+        {
+            return Enum.GetName(typeof(ClubJoinRequestStatus), Status);
+        }
     }
 
-    private void Validate()
+    public enum ClubJoinRequestStatus
     {
-        if (TouristId == 0) throw new ArgumentException("Invalid TouristId");
-        if (ClubId == 0) throw new ArgumentException("Invalid ClubId");
-        if (RequestedAt > DateTime.Now) throw new ArgumentException("Invalid RequestedAt");
+        Pending,
+        Accepted,
+        Rejected,
+        Cancelled
     }
-
-    public void Respond(bool accepted)
-    {
-        if (Status == ClubJoinRequestStatus.Pending)
-            Status = accepted ? ClubJoinRequestStatus.Accepted : ClubJoinRequestStatus.Rejected;
-    }
-
-    public void Cancel()
-    {
-        if (Status == ClubJoinRequestStatus.Pending)
-            Status = ClubJoinRequestStatus.Cancelled;
-    }
-
-    public string GetPrimaryStatusName()
-    {
-        return Enum.GetName(typeof(ClubJoinRequestStatus), Status);
-    }
-}
-
-public enum ClubJoinRequestStatus
-{
-Pending,
-Accepted,
-Rejected,
-Cancelled
 }
