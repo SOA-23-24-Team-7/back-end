0b9eafdd270290765faff9ea1913194c0642d81e
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-10-24 23:12:12 +0200

    Added test for club memberships

--- src/Explorer.API/Controllers/Tourist/ClubMemberManagementController.cs
+++ src/Explorer.API/Controllers/Tourist/ClubMemberManagementController.cs
@@ -1,4 +1,6 @@
-﻿using Explorer.Stakeholders.API.Public;
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -17,17 +19,17 @@ public class ClubMemberManagementController : BaseApiController
     }
 
     [HttpDelete("kick/{id:int}")]
-    public ActionResult KickTourist(int id)
+    public ActionResult KickTourist(int membershipId)
     {
         var userId = extractUserIdFromHttpContext();
-        var result = _clubMemberManagementService.KickTourist(id, userId);
+        var result = _clubMemberManagementService.KickTourist(membershipId, userId);
         return CreateResponse(result);
     }
 
     [HttpGet("{id:long}")]
-    public ActionResult GetMembers(long id)
+    public ActionResult<PagedResult<ClubMemberDto>> GetMembers(long clubId)
     {
-        var members = _clubMemberManagementService.GetMembers(id);
+        var members = _clubMemberManagementService.GetMembers(clubId);
         return CreateResponse(members);
     }
 
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/Integration/ClubMemberships/ClubMembershipsQueryTests.cs
@@ -0,0 +1,59 @@
+﻿using Explorer.API.Controllers.Tourist;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Stakeholders.Tests.Integration.ClubMemberships;
+
+public class ClubMembershipsQueryTests : BaseStakeholdersIntegrationTest
+{
+    public ClubMembershipsQueryTests(StakeholdersTestFactory factory) : base(factory) { }
+
+    [Fact]
+    public void Retrieves_club_members()
+    {
+        // Arange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var clubId = -2;
+        var page = 1;
+        var pageSize = 10;
+
+        // Act
+        var result = ((ObjectResult)controller.GetMembers(clubId).Result!).Value as PagedResult<ClubMemberDto>;
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.Results.Count.ShouldBe(2);
+        result.TotalCount.ShouldBe(2);
+    }
+
+    [Fact]
+    public void Retrieves_club_members_invalid_clubId()
+    {
+        // Arange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var clubId = 0;
+        var page = 1;
+        var pageSize = 10;
+
+        // Act
+        var result = (ObjectResult)controller.GetMembers(clubId).Result!;
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(404);
+    }
+
+    private static ClubMemberManagementController CreateController(IServiceScope scope)
+    {
+        return new ClubMemberManagementController(scope.ServiceProvider.GetRequiredService<IClubMemberManagementService>())
+        {
+            ControllerContext = BuildContext("-1")
+        };
+    }
+}
