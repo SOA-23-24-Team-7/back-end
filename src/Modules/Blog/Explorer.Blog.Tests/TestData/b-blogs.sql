7d408568a5aad73eb52b19c30c41be516eecb14b
markok4 <markokrstic001@gmail.com>
2023-10-24 20:45:51 +0200

    test: Fixed queryBlogTest

--- src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
+++ src/Modules/Blog/Explorer.Blog.Tests/TestData/b-blogs.sql
@@ -1,9 +1,9 @@
 ﻿INSERT INTO blog."Blogs" ("Title", "Description", "Date", "Pictures", "Status") 
 VALUES ('Naslov Bloga1', 'Ovo je opis bloga1.', '2001-01-01', 
-        ARRAY(), 1);
+        NULL, 1);
         INSERT INTO blog."Blogs" ("Title", "Description", "Date", "Pictures", "Status") 
 VALUES ('Naslov Bloga2', 'Ovo je opis bloga2.', '2001-01-01', 
-        ARRAY('slika1.jpg', 'slika2.jpg'), 1);
+        ARRAY['slika1.jpg', 'slika2.jpg'], 1);
         INSERT INTO blog."Blogs" ("Title", "Description", "Date", "Pictures", "Status") 
 VALUES ('Naslov Bloga3', 'Ovo je opis bloga3.', '2001-01-01', 
-        ARRAY('slika1.jpg', 'slika2.jpg'), 1);
+        ARRAY['slika1.jpg', 'slika2.jpg'], 1);
