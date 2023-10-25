3e8c3f9ca1a3d22120148f9048c7a2af997e4e0d
mvelimir <velimirmilinkovic1@gmail.com>
2023-10-25 08:30:41 +0200

    test: Update and delete key point tests

---
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/MarketPlace/KeyPointQueryTests.cs
@@ -0,0 +1,37 @@
+﻿using Explorer.API.Controllers.Tourist.MarketPlace;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.TourAuthoring;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+
+namespace Explorer.Tours.Tests.Integration.MarketPlace;
+
+[Collection("Sequential")]
+public class KeyPointQueryTests : BaseToursIntegrationTest
+{
+    public KeyPointQueryTests(ToursTestFactory factory) : base(factory) { }
+
+    [Fact]
+    public void Retrieves_all_for_single_tour()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+
+        // Act
+        var result = ((ObjectResult)controller.GetKeyPoints(-1).Result)?.Value as List<KeyPointDto>;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.Count.ShouldBe(2);
+    }
+
+    private static KeyPointController CreateController(IServiceScope scope)
+    {
+        return new KeyPointController(scope.ServiceProvider.GetRequiredService<IKeyPointService>())
+        {
+            ControllerContext = BuildContext("-1")
+        };
+    }
+}
--- src/Modules/Tours/Explorer.Tours.Tests/Integration/TourAuthoring/KeyPointCommandTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/TourAuthoring/KeyPointCommandTests.cs
@@ -26,11 +26,12 @@ public class KeyPointCommandTests : BaseToursIntegrationTest
             Description = "Kej na obali Dunava.",
             Longitude = 12.542,
             Latitude = 54.3221,
-            ImagePath = "kej.png"
+            ImagePath = "kej.png",
+            Order = 2
         };
 
         // Act
-        var result = ((ObjectResult)controller.CreateKeyPoint(1, newEntity).Result)?.Value as KeyPointDto;
+        var result = ((ObjectResult)controller.CreateKeyPoint(-1, newEntity).Result)?.Value as KeyPointDto;
 
         // Assert - Response
         result.ShouldNotBeNull();
@@ -62,9 +63,114 @@ public class KeyPointCommandTests : BaseToursIntegrationTest
         result.StatusCode.ShouldBe(400);
     }
 
-    private static TourController CreateController(IServiceScope scope)
+    [Fact]
+    public void Updates()
     {
-        return new TourController(scope.ServiceProvider.GetRequiredService<IKeyPointService>())
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
+        var updatedEntity = new KeyPointDto
+        {
+            Id = -1,
+            TourId = -1,
+            Name = "Rimokatolicka crkva Ime Marijino",
+            Description = "Rimokatolicka crkva u centru grada",
+            Longitude = -12.643,
+            Latitude = 78.54,
+            ImagePath = "new-image.png",
+            Order = 0
+        };
+
+        // Act
+        var result = ((ObjectResult)controller.Update(-1, -1, updatedEntity).Result)?.Value as KeyPointDto;
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(-1);
+        result.Name.ShouldBe(updatedEntity.Name);
+        result.Description.ShouldBe(updatedEntity.Description);
+        result.Longitude.ShouldBe(updatedEntity.Longitude);
+        result.Latitude.ShouldBe(updatedEntity.Latitude);
+        result.ImagePath.ShouldBe(updatedEntity.ImagePath);
+        result.Order.ShouldBe(updatedEntity.Order);
+
+        // Assert - Database
+        var storedEntity = dbContext.KeyPoints.FirstOrDefault(i => i.Name == updatedEntity.Name);
+        storedEntity.ShouldNotBeNull();
+        storedEntity.Description.ShouldBe(updatedEntity.Description);
+        storedEntity.Longitude.ShouldBe(updatedEntity.Longitude);
+        storedEntity.Latitude.ShouldBe(updatedEntity.Latitude);
+        storedEntity.ImagePath.ShouldBe(updatedEntity.ImagePath);
+        storedEntity.Order.ShouldBe(updatedEntity.Order);
+        var oldEntity = dbContext.KeyPoints.FirstOrDefault(i => i.Name == "Katedrala");
+        oldEntity.ShouldBeNull();
+    }
+
+    [Fact]
+    public void Update_fails_invalid_id()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var updatedEntity = new KeyPointDto
+        {
+            Id = -1000,
+            TourId = -1,
+            Name = "Test",
+            Description = "Test",
+            Longitude = 0,
+            Latitude = 0,
+            ImagePath = "Test",
+            Order = 0
+        };
+
+        // Act
+        var result = (ObjectResult)controller.Update(-1, -1000, updatedEntity).Result;
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(404);
+    }
+
+    [Fact]
+    public void Deletes()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+        var dbContext = scope.ServiceProvider.GetRequiredService<ToursContext>();
+
+        // Act
+        var result = (OkResult)controller.Delete(-1, -1);
+
+        // Assert - Response
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(200);
+
+        // Assert - Database
+        var storedCourse = dbContext.KeyPoints.FirstOrDefault(i => i.Id == -3);
+        storedCourse.ShouldBeNull();
+    }
+
+    [Fact]
+    public void Delete_fails_invalid_id()
+    {
+        // Arrange
+        using var scope = Factory.Services.CreateScope();
+        var controller = CreateController(scope);
+
+        // Act
+        var result = (ObjectResult)controller.Delete(-1, -1000);
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.StatusCode.ShouldBe(404);
+    }
+
+    private static KeyPointController CreateController(IServiceScope scope)
+    {
+        return new KeyPointController(scope.ServiceProvider.GetRequiredService<IKeyPointService>())
         {
             ControllerContext = BuildContext("-1")
         };
