b6722eaa7b1894e46fabca98c4e48098881fa936
IvanPartalo <ivanpartalo123@gmail.com>
2023-11-14 19:30:13 +0100

    fix: Update KeyPoint

--- src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/KeyPoint.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/KeyPoint.cs
@@ -16,6 +16,8 @@ public class KeyPoint : Entity
     public bool HaveSecret { get; init; }
     public KeyPointSecret? Secret { get; private set; }
 
+    public KeyPoint() { }
+
     public KeyPoint(long tourId, string name, string description, double longitude, double latitude, string locationAddress, string imagePath, long order, KeyPointSecret? secret)
     {
         TourId = tourId;
