08a66c96f001e54ddcebee66c4fc2bb1c6c215e3
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-11-21 12:20:33 +0100

    Moved some classes to payments module

---
+++ src/Explorer.Payments.API/Public/ITourTokenService.cs
@@ -0,0 +1,10 @@
+﻿using Explorer.Payments.API.Dtos;
+using FluentResults;
+
+namespace Explorer.Payments.API.Public;
+
+public interface ITourTokenService
+{
+    Result<TourTokenResponseDto> AddToken(TourTokenCreateDto token);
+    Result<List<TourTokenResponseDto>> GetTouristsTokens(long touristId);
+}
--- src/Explorer.Payments.Core/Mappers/PaymentsProfile.cs
+++ src/Explorer.Payments.Core/Mappers/PaymentsProfile.cs
@@ -1,7 +1,19 @@
 ﻿using AutoMapper;
+using Explorer.Payments.API.Dtos;
+using Explorer.Payments.Core.Domain.ShoppingCarts;
 
 namespace Explorer.Payments.Core.Mappers;
 
 public class PaymentsProfile : Profile
 {
+    public PaymentsProfile()
+    {
+        CreateMap<ShoppingCartResponseDto, ShoppingCart>().ReverseMap().ForMember(x => x.OrderItems, opt => opt.MapFrom(src => src.OrderItems));
+        CreateMap<ShoppingCartCreateDto, ShoppingCart>().ReverseMap();
+        CreateMap<ShoppingCartUpdateDto, ShoppingCart>().ReverseMap();
+
+        CreateMap<OrderItemResponseDto, OrderItem>().ReverseMap();
+        CreateMap<OrderItemCreateDto, OrderItem>().ReverseMap();
+        CreateMap<OrderItemUpdateDto, OrderItem>().ReverseMap();
+    }
 }
