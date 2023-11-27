a378704301e5c9615ec79527b783951f2473828f
ukinolo <ykithethird@gmail.com>
2023-11-27 13:03:35 +0100

    test: Added test for wallet

---
+++ src/Explorer.API/Controllers/WalletControler.cs
@@ -0,0 +1,26 @@
+﻿using Explorer.Payments.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Route("api/wallet")]
+    public class WalletController : BaseApiController
+    {
+        public WalletController() { }
+
+        [Authorize(Policy = "touristPolicy")]
+        [HttpGet]
+        public ActionResult<WalletResponseDto> GetTouristsWallet()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Policy = "administratorPolicy")]
+        [HttpPut("{id:long}")]
+        public ActionResult<WalletResponseDto> UpdateWallet([FromBody] WalletUpdateDto walletUpdateDto)
+        { 
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Modules/Payments/Explorer.Payments.Tests/Integration/WalletCommandTest.cs
@@ -0,0 +1,76 @@
+﻿using Explorer.API.Controllers.Tourist;
+using Explorer.Payments.API.Dtos;
+using Explorer.Payments.Infrastructure.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System.Security.Claims;
+
+namespace Explorer.Payments.Tests.Integration
+{
+    public class WalletCommandTest : BasePaymentsIntegrationTest
+    {
+        public WalletCommandTest(PaymentsTestFactory factory) : base(factory) { }
+
+        [Fact]
+        public void Update()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<PaymentsContext>();
+
+            var newEntity = new WalletUpdateDto
+            {
+                Id = -1,
+                AdventureCoin = 1,
+            };
+
+            var result = ((ObjectResult)controller.UpdateWallet(newEntity).Result)?.Value as WalletResponseDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+            result.Id.ShouldNotBe(-1);
+            result.AdventureCoin.ShouldBe(newEntity.AdventureCoin);
+
+            // Assert - Database
+            //TODO
+        }
+
+        [Fact]
+        public void Get()
+        {
+            // Arrange
+            using var scope = Factory.Services.CreateScope();
+            var controller = CreateController(scope);
+            var dbContext = scope.ServiceProvider.GetRequiredService<PaymentsContext>();
+
+            var contextUser = new ClaimsIdentity(new Claim[] { new Claim("id", "1") }, "test");
+            var context = new DefaultHttpContext()
+            {
+                User = new ClaimsPrincipal(contextUser)
+            };
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = context
+            };
+
+            var result = ((ObjectResult)controller.GetTouristsWallet().Result)?.Value as WalletResponseDto;
+
+            // Assert - Response
+            result.ShouldNotBeNull();
+
+            // Assert - Database
+            //TODO
+        }
+
+        private static WalletController CreateController(IServiceScope scope)
+        {
+            return new WalletController()
+            {
+                ControllerContext = BuildContext("-1")
+            };
+        }
+    }
+}
