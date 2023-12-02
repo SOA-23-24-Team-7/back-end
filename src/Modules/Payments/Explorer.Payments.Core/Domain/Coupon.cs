d74c860348cb4e5f0c3470b05b6cdc2d205a5647
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2023-12-02 19:08:26 +0100

    feat: Coupon code generating

---
+++ src/Modules/Payments/Explorer.Payments.Core/Domain/Coupon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.BuildingBlocks.Core.Domain;
+using Explorer.Tours.Core.Domain.Tours;
+
+namespace Explorer.Payments.Core.Domain
+{
+    public class Coupon : Entity
+    {
+        public string Code { get; init; }
+        public string Discount { get;init; }
+        public long TourId { get; init; }
+        public DateTime ExpirationDate { get; init; }
+        public bool AllFromAuthor { get; init; }    //ako se checkbox odabere da vazi za sve ture tog autora
+
+        public Coupon(string discount, long tourId, DateTime expirationDate, bool allFromAuthor)
+        {
+            Discount = discount;
+            TourId = tourId;
+            ExpirationDate = expirationDate;
+            AllFromAuthor = allFromAuthor;
+            Code = GenerateCode();
+        }
+        private string GenerateCode()
+        {
+            Random random = new Random();
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            int length = 8;
+            StringBuilder code = new StringBuilder(length);
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(characters.Length);
+                code.Append(characters[index]);
+            }
+
+            return code.ToString();
+
+        }
+    }
+}
