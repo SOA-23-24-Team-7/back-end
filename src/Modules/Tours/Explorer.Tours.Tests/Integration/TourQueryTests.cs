5f239f7e5eca83ba8fe46c739407646985b14aea
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2023-12-03 21:14:27 +0100

    test: Tour test fixed

--- src/Modules/Tours/Explorer.Tours.Tests/Integration/TourQueryTests.cs
+++ src/Modules/Tours/Explorer.Tours.Tests/Integration/TourQueryTests.cs
@@ -25,8 +25,8 @@ public class TourQueryTests : BaseToursIntegrationTest
 
         // Assert
         result.ShouldNotBeNull();
-        result.Results.Count.ShouldBe(8);
-        result.TotalCount.ShouldBe(8);
+        result.Results.Count.ShouldBe(12);
+        result.TotalCount.ShouldBe(12);
     }
 
     [Fact]
@@ -41,8 +41,8 @@ public class TourQueryTests : BaseToursIntegrationTest
 
         // Assert
         result.ShouldNotBeNull();
-        result.Results.Count.ShouldBe(2);
-        result.TotalCount.ShouldBe(2);
+        result.Results.Count.ShouldBe(6);
+        result.TotalCount.ShouldBe(6);
     }
 
     [Fact]
