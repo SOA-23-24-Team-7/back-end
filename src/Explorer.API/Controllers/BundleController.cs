92e66e9064b17ee1e23921a10d139df313f50a88
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-12-05 13:34:14 +0100

    Added methods to controller

--- src/Explorer.API/Controllers/BundleController.cs
+++ src/Explorer.API/Controllers/BundleController.cs
@@ -1,6 +1,50 @@
-﻿namespace Explorer.API.Controllers
+﻿using Explorer.Payments.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers
 {
-    public class BundleController
+    [Route("api/bundles")]
+    public class BundleController : BaseApiController
     {
+        public BundleController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult<string> Create([FromBody] BundleCreationDto dto)
+        {
+            return null;
+        }
+
+        [HttpPatch("publish/{id:long}")]
+        public ActionResult<string> Publish(long id)
+        {
+            return null;
+        }
+
+        [HttpPatch("delete/{id:long}")]
+        public ActionResult<string> Delete(long id)
+        {
+            return null;
+        }
+
+        [HttpPatch("archive/{id:long}")]
+        public ActionResult<string> Archive(long id)
+        {
+            return null;
+        }
+
+        [HttpPatch("add-items/{id:long}")]
+        public ActionResult<string> AddItems(long id, [FromBody] string tourIds)
+        {
+            return null;
+        }
+
+        [HttpPatch("remove-items/{id:long}")]
+        public ActionResult<string> RemoveItems(long id, [FromBody] string tourIds)
+        {
+            return null;
+        }
     }
 }
