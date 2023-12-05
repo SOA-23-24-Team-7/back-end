f50b3cab7e51ce702e8a1c6b3dea0f6c641a6c0d
Veljko121 <109314562+Veljko121@users.noreply.github.com>
2023-12-05 12:54:08 +0100

    Added domain classes

---
+++ src/Modules/Payments/Explorer.Payments.Core/Domain/Bundles/Bundle.cs
@@ -0,0 +1,52 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Core.Domain.Bundles;
+
+public class Bundle : Entity
+{
+    public string Name { get; init; }
+    public int Price { get; init; }
+    public BundleStatus Status { get; private set; }
+    public List<BundleItem> BundleItems { get; init; }
+
+    public Bundle() { }
+
+    public Bundle(string name, int price, BundleStatus status, List<BundleItem> bundleItems)
+    {
+        if (price < 0) throw new ArgumentException("Invalid price.");
+        if (name  == null || name == "") throw new ArgumentException("Invalid name.");
+
+        Name = name;
+        Price = price;
+        Status = status;
+        BundleItems = bundleItems;
+    }
+
+    public void Publish()
+    {
+        if (Status != BundleStatus.Deleted) Status = BundleStatus.Published;
+    }
+
+    public void Delete()
+    {
+        if (Status != BundleStatus.Published) Status = BundleStatus.Deleted;
+    }
+
+    public void Archive()
+    {
+        if (Status == BundleStatus.Published) Status = BundleStatus.Archived;
+    }
+}
+
+public enum BundleStatus
+{
+    Draft,
+    Published,
+    Archived,
+    Deleted
+}
---
+++ src/Modules/Payments/Explorer.Payments.Core/Domain/Bundles/BundleItem.cs
@@ -0,0 +1,22 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Core.Domain.Bundles;
+
+public class BundleItem : Entity
+{
+    public long BundleId { get; init; }
+    public long TourId { get; init; }
+
+    public BundleItem() { }
+
+    public BundleItem(long bundleId, long tourId)
+    {
+        BundleId = bundleId;
+        TourId = tourId;
+    }
+}
