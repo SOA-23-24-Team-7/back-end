c8373618ef5205986c90925f41df86687f05b33a
fsimic <fsimic346@gmail.com>
2023-12-06 16:20:27 +0100

    feat: Added encounter create endpoints for tourists

--- src/Explorer.API/Controllers/Tourist/HiddenLocationEncounterController.cs
+++ src/Explorer.API/Controllers/Tourist/HiddenLocationEncounterController.cs
@@ -1,6 +1,7 @@
 ﻿using Explorer.Encounters.API.Dtos;
 using Explorer.Encounters.API.Public;
 using Explorer.Tours.API.Dtos.TouristPosition;
+using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,9 +12,11 @@ namespace Explorer.API.Controllers.Tourist
     public class HiddenLocationEncounterController : BaseApiController
     {
         private readonly IEncounterService _encounterService;
-        public HiddenLocationEncounterController(IEncounterService encounterService)
+        private readonly ITouristProgressService _touristProgressService;
+        public HiddenLocationEncounterController(IEncounterService encounterService, ITouristProgressService touristProgressService)
         {
             _encounterService = encounterService;
+            _touristProgressService = touristProgressService;
         }
 
         [HttpGet("{id:long}")]
@@ -29,6 +32,20 @@ namespace Explorer.API.Controllers.Tourist
             long userId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
             var result = _encounterService.CompleteHiddenLocationEncounter(userId, id, position.Longitude, position.Latitude);
             return CreateResponse(result);
+        }
+
+        [HttpPost("create")]
+        public ActionResult<HiddenLocationEncounterResponseDto> Create([FromBody] HiddenLocationEncounterCreateDto encounter)
+        {
+            long userId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
+            var progress = _touristProgressService.GetByUserId(userId);
+            if (progress.Value.Level >= 10)
+            {
+                var result = _encounterService.CreateHiddenLocationEncounter(encounter);
+                return CreateResponse(result);
+
+            }
+            return CreateResponse(Result.Fail("Tourist level is not high enough."));
         }
     }
 }
