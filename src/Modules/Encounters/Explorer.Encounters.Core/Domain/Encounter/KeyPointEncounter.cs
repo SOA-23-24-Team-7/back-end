1917aa8946ddad6a1300a451fb73044cecfa378b
gagi155 <maslac.ra151.2020@gmail.com>
2023-12-06 23:56:26 +0100

    reafactor

--- src/Modules/Encounters/Explorer.Encounters.Core/Domain/Encounter/KeyPointEncounter.cs
+++ src/Modules/Encounters/Explorer.Encounters.Core/Domain/Encounter/KeyPointEncounter.cs
@@ -4,7 +4,7 @@ public class KeyPointEncounter : Encounter
 {
     public long KeyPointId { get; init; }
 
-    public KeyPointEncounter(string title, string description, double longitude, double latitude, double radius, int xpReward, EncounterStatus status, long keyPointId) : base(title, description, longitude, latitude, radius, xpReward, status)
+    public KeyPointEncounter(string title, string description, double longitude, double latitude, double radius, int xpReward, EncounterStatus status, long keyPointId) : base(title, description, longitude, latitude, radius, xpReward, status, EncounterType.KeyPoint)
     {
         KeyPointId = keyPointId;
     }
