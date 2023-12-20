65f27a289a28613c0cedbce0c5d5077d8a037ff5
mvelimir <velimirmilinkovic1@gmail.com>
2023-12-20 14:00:01 +0100

    feat: Include profile picture in rating dto

--- src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/RatingWithUserDto.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/RatingWithUserDto.cs
@@ -8,5 +8,6 @@
         public DateTime DateTime { get; set; }
         public long UserId { get; set; }
         public string UserName { get; set; }
+        public string? ProfilePicture { get; set; }
     }
 }
