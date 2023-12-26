dd0d6ca001b6a0274b57a18ec51ee9309e67a498
Jelena Kovač <jelena.kovac765@gmail.com>
2023-12-26 17:48:17 +0100

    feat: adding domain class for notification

---
+++ src/Modules/Payments/Explorer.Payments.Core/Domain/WishlistNotification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Core.Domain
+{
+    public class WishlistNotification
+    {
+        public long TourId { get; init; }
+        public string Description { get; private set; }
+        public long TouristId { get; init; }
+
+        public WishlistNotification(long tourId, string description, long touristId)
+        {
+            TourId = tourId;
+            Description = description;
+            TouristId = touristId;
+        }
+
+
+    }
+}
