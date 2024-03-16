488e1ed5801da35c608b52fa3cc92c084ea2dff1
mvelimir <velimirmilinkovic1@gmail.com>
2024-03-16 20:36:38 +0100

    feat: Tour preference creation and retrieval

--- src/Explorer.API/Controllers/Tourist/PreferenceController.cs
+++ src/Explorer.API/Controllers/Tourist/PreferenceController.cs
@@ -3,6 +3,12 @@ using Explorer.Tours.API.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Explorer.BuildingBlocks.Infrastructure.HTTP.Interfaces;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.Core.Domain.Tours;
+using System.Text.Json;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace Explorer.API.Controllers.Tourist
 {
@@ -10,48 +16,71 @@ namespace Explorer.API.Controllers.Tourist
     [Route("api/tourist/preferences")]
     public class PreferenceController : BaseApiController
     {
-        private readonly IPreferenceService _tourPreferencesService;
+        private readonly IHttpClientService _httpClient;
 
-        public PreferenceController(IPreferenceService tourPreferencesService)
+        public PreferenceController(IHttpClientService httpClient)
         {
-            _tourPreferencesService = tourPreferencesService;
+            _httpClient = httpClient;
         }
 
         [HttpGet]
-        public ActionResult<PreferenceResponseDto> Get()
+        public async Task<ActionResult<PreferenceResponseDto>> Get()
         {
-           // int userId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
             int id = 0;
             var identity = HttpContext.User.Identity as ClaimsIdentity;
             if (identity != null && identity.IsAuthenticated)
             {
                 id = int.Parse(identity.FindFirst("id").Value);
             }
-            var result = _tourPreferencesService.GetByUserId(id);
-            return CreateResponse(result);
+
+            string uri = _httpClient.BuildUri(Protocol.HTTP, "localhost", 8087, "tourists/" + id + "/tour-preference");
+
+            var response = await _httpClient.GetAsync(uri);
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var res = JsonSerializer.Deserialize<PreferenceResponseDto>(jsonString);
+                return CreateResponse(FluentResults.Result.Ok(res));
+            }
+            else
+            {
+                return CreateResponse(FluentResults.Result.Fail(FailureCode.InvalidArgument));
+            }
         }
 
         [HttpPost("create")]
-        public ActionResult<PreferenceResponseDto> Create([FromBody] PreferenceCreateDto preference)
+        public async Task<ActionResult<PreferenceResponseDto>> Create([FromBody] PreferenceCreateDto preference)
         {
             preference.UserId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
-            var result = _tourPreferencesService.Create(preference);
-            return CreateResponse(result);
+
+            string uri = _httpClient.BuildUri(Protocol.HTTP, "localhost", 8087, "tour-preferences");
+
+            string requestBody = JsonSerializer.Serialize(preference);
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(uri, content);
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var res = JsonSerializer.Deserialize<PreferenceResponseDto>(jsonString);
+                return CreateResponse(FluentResults.Result.Ok(res));
+            }
+            else
+            {
+                return CreateResponse(FluentResults.Result.Fail(FailureCode.InvalidArgument));
+            }
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
-            var result = _tourPreferencesService.Delete(id);
-            return CreateResponse(result);
+            throw new NotImplementedException();
         }
 
         [HttpPut]
         public ActionResult<PreferenceResponseDto> Update([FromBody] PreferenceUpdateDto preference)
         {
-            preference.UserId = int.Parse(HttpContext.User.Claims.First(i => i.Type.Equals("id", StringComparison.OrdinalIgnoreCase)).Value);
-            var result = _tourPreferencesService.Update(preference);
-            return CreateResponse(result);
+            throw new NotImplementedException();
         }
     }
 }
