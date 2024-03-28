4992148d0372b76e7d2fe67fafee9ed161792fb9
mvelimir <velimirmilinkovic1@gmail.com>
2024-03-28 11:12:43 +0100

    fix: Enable Swagger in release mode

--- src/Explorer.API/Program.cs
+++ src/Explorer.API/Program.cs
@@ -20,14 +20,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 else
 {
     app.UseExceptionHandler("/error");
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseRouting();
 app.UseCors(corsPolicy);
