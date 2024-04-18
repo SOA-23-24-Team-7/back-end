cf2e5c320679dfbd596bd7a0d48dc3aaad44d188
kirdatatjana <116630536+kirdatatjana@users.noreply.github.com>
2024-04-18 22:03:08 +0100

    connected controller to followers microservice

--- src/Explorer.API/Controllers/FollowerController.cs
+++ src/Explorer.API/Controllers/FollowerController.cs
@@ -1,4 +1,6 @@
-﻿using Explorer.BuildingBlocks.Core.UseCases;
+﻿using Explorer.Blog.API.Dtos;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.BuildingBlocks.Infrastructure.HTTP.Interfaces;
 using Explorer.Stakeholders.API.Dtos;
 using Explorer.Stakeholders.API.Public;
 using Explorer.Stakeholders.Core.Domain;
@@ -6,7 +8,9 @@ using Explorer.Stakeholders.Core.UseCases;
 using FluentResults;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Security.Claims;
+using System.Text;
 
 namespace Explorer.API.Controllers
 {
@@ -16,10 +20,13 @@ namespace Explorer.API.Controllers
     {
         private readonly IFollowerService _followerService;
         private readonly IUserService _userService;
-        public FollowerController(IFollowerService followerService, IUserService userService)
+
+        private readonly IHttpClientService _httpClientService;
+        public FollowerController(IFollowerService followerService, IUserService userService, IHttpClientService httpClientService)
         {
             _followerService = followerService;
             _userService = userService;
+            _httpClientService = httpClientService;
         }
 
         [HttpGet("followers/{id:long}")]
@@ -34,6 +41,8 @@ namespace Explorer.API.Controllers
             var result = _followerService.GetFollowers(page, pageSize, userId);
             return CreateResponse(result);
         }
+
+
         [HttpGet("followings/{id:long}")]
         public ActionResult<PagedResult<FollowingResponseWithUserDto>> GetFollowings([FromQuery] int page, [FromQuery] int pageSize, long id)
         {
@@ -72,6 +81,78 @@ namespace Explorer.API.Controllers
             }
             var result = _userService.SearchUsers(0, 0, searchUsername, userId);
             return CreateResponse(result);
+        }
+
+        [HttpPost("follow")]
+        public async Task<ActionResult> Follow([FromBody] FollowerCreateDto follower)
+        {
+            string uri = _httpClientService.BuildUri(Protocol.HTTP, "localhost", 8095, $"followers/follow/{follower.UserId}/{follower.FollowedById}");
+           
+            var response = await _httpClientService.PostAsync(uri,null);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return CreateResponse(Result.Ok(content));
+            }
+            else
+            {
+                return StatusCode(500, "Error following");
+            }
+        }
+
+
+
+        [HttpPost("unfollow")]
+        public async Task<ActionResult> Unfollow([FromBody] FollowerCreateDto follower)
+        {
+            string uri = _httpClientService.BuildUri(Protocol.HTTP, "localhost", 8095, $"followers/unfollow/{follower.UserId}/{follower.FollowedById}");
+
+            var response = await _httpClientService.PostAsync(uri, null);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return CreateResponse(Result.Ok(content));
+            }
+            else
+            {
+                return StatusCode(500, "Error following");
+            }
+        }
+
+        [HttpGet("getFollowers/{id:long}")]
+        public async Task<String> GetAllFollowers(long id)
+        {
+            string uri = _httpClientService.BuildUri(Protocol.HTTP, "localhost", 8095, $"followers/getFollowers/{id}");
+            var response = await _httpClientService.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpGet("getFollowings/{id:long}")]
+        public async Task<String> GetAllFollowings(long id)
+        {
+            string uri = _httpClientService.BuildUri(Protocol.HTTP, "localhost", 8095, $"followers/getFollowings/{id}");
+            var response = await _httpClientService.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 
