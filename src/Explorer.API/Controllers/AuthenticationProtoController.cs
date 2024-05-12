1acf721d7bb0888b4a1ce370d7eac168c8bb7524
kirdatatjana <116630536+kirdatatjana@users.noreply.github.com>
2024-05-12 11:56:20 +0100

    Added dependencies for gRPC & implementation start

---
+++ src/Explorer.API/Controllers/AuthenticationProtoController.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServiceTranscoding;
+
+
+namespace Explorer.API.Controllers
+{
+    public class AuthenticationProtoController : Authorize.AuthorizeBase
+    {
+        private readonly ILogger<AuthenticationProtoController> _logger;
+
+        public AuthenticationProtoController(ILogger<AuthenticationProtoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<AuthenticationTokens> Authorize(Credentials request,
+            ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("https://localhost:44332", new GrpcChannelOptions { HttpHandler = httpHandler });
+            //mislim da treba da bude https://localhost:44333 ?
+
+
+            var client = new Authorize.AuthorizeClient(channel);
+            var response = await client.AuthorizeAsync(request);
+
+            Console.WriteLine(response.AccessToken);
+
+            return await Task.FromResult(new AuthenticationTokens
+            {
+                Id = response.Id,
+                AccessToken = response.AccessToken
+            });
+        }
+    }
+}
